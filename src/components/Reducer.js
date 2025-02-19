e72511f20687397cf36d32d79457a32ed52a3612
deepankjadav <jadavdeepank@gmail.com>
2025-02-19 11:36:44 +0530

    Update Reducer.js

--- src/components/Reducer.js
+++ src/components/Reducer.js
@@ -1,19 +1,1 @@
 
-
-const INCREMENT = "INCREMENT";
-const DECEREMENT = "DECEREMENT";
-
-const initialState = { count: 0 };
-
-const Reducer = (state=initialState, actions) => {
-    switch(actions.type){
-        case INCREMENT:
-            return {count : state.count + 1};
-        case DECEREMENT:
-            return {count : state.count - 1};
-        default:
-            return state;
-    }
-}
-
-export default Reducer;
